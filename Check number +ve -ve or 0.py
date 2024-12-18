b5085296dd48a9c6a7f5c0d1723a528d9ad51ae6
Shubham Mahindrakar <shubhammahindrakar2104@gmail.com>
2024-12-19 01:41:42 +0530

    Continued

---
+++ Check number +ve -ve or 0.py
@@ -0,0 +1,11 @@
+#Program to check if a number is positive or negative or zero
+
+Number = float(input("Enter the Number: "))
+
+if Number > 0:
+    print("It is a positive number")
+elif Number == 0:
+    print("Number is Zero")
+else:
+    print("It is a Negative number") 
+            
---
+++ Convert Km to Miles.py
@@ -0,0 +1,8 @@
+#Solution1
+#Multiplication by 0.621371
+
+KM = float(input("Enter your value in Kms: "))
+
+Miles = (0.621371) * KM
+
+print(KM, "Kms in miles will be", Miles, "Miles")
